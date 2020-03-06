41d176769ef3e752be1dbe08de65336a3d958a0a
Debora <debora.goncalves@vigil365.com>
2020-03-06 15:05:38 +0000

    fixes

--- src/components/slider/events/index.tsx
+++ src/components/slider/events/index.tsx
@@ -17,19 +17,18 @@ class SliderEventsComponent extends React.Component<Props> {
   render(): React.ReactNode {
     return (
       <div className="row slider image">
-        
-          <div className={'row content'}>
-            <div className="col s8">
-              <h1 className="title">{this.props.title}</h1>
-            </div>
-            <div className="col s4 view-all">
-              {this.props.viewAll && (
-                <Button color={'transparent'} label={'View All'} />
-              )}
-            </div>
+        <div className={'row content'}>
+          <div className="col s8">
+            <h1 className="title">{this.props.title}</h1>
           </div>
-          <div className="row">
-            <div className="col s12">
+          <div className="col s4 view-all">
+            {this.props.viewAll && (
+              <Button color={'transparent'} label={'View All'} />
+            )}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s12">
             {_.map(
               this.props.data,
               (data, i): React.ReactNode => {

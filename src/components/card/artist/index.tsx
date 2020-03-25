0fe46f280ea5c22ff52d89b07a6b4f85b1ccfa8f
Felipe Valle <felipe.valle@vigil365.com>
2020-03-25 08:18:33 -0300

    card-artist

--- src/components/card/artist/index.tsx
+++ src/components/card/artist/index.tsx
@@ -31,59 +31,49 @@ class CardArtistComponent extends React.Component<Props> {
     const { cover, username, support, name } = artist;
 
     return (
-      <div className="row">
-        <div className="col s12">
-          <div
-            className="card artist"
-            style={{ backgroundImage: `url(${cover.main})` }}
-          >
+      <div
+        className="card card-artist"
+        style={{ backgroundImage: `url(${cover.main})` }}
+      >
+        <div className="row">
+          <div className="col s12 infos p-10">
             <div className="row">
-              <div
-                className="card-area"
-                onClick={(): void => {
-                  history.push(`/home/artist/${username}`);
-                }}
-              ></div>
-              <div className="col s12 infos p-10">
-                <div className="row">
-                  <div className="col s12 button">
-                    <ButtonIcon
-                      color={Colors.transparent}
-                      icon={<DotsThreeIcon color={'#6a6565'} />}
-                      onClick={updateSettingsModal.bind(
-                        this,
-                        true,
-                        React.createElement(MenuFanSupportOptions, {
-                          artist: artist,
-                          onClick: updateSettingsModal.bind(this, false, null),
-                          background: 'background-tertiary-opacity95'
-                        }),
-                        'background-tertiary-opacity95'
-                      )}
-                    />
-                  </div>
+              <div className="col s12 button">
+                <ButtonIcon
+                  color={Colors.transparent}
+                  icon={<DotsThreeIcon color={'#6a6565'} />}
+                  onClick={updateSettingsModal.bind(
+                    this,
+                    true,
+                    React.createElement(MenuFanSupportOptions, {
+                      artist: artist,
+                      onClick: updateSettingsModal.bind(this, false, null),
+                      background: 'background-tertiary-opacity95'
+                    }),
+                    'background-tertiary-opacity95'
+                  )}
+                />
+              </div>
+            </div>
+            <div
+              className="row"
+              onClick={(): void => {
+                history.push(`/home/artist/${username}`);
+              }}
+            >
+              <div className="col s8 align-items-end">
+                <div className="name">
+                  <span>{name}</span>
                 </div>
-                <div
-                  className="row"
+              </div>
+              <div className="col s4 support">
+                <ButtonSupport
+                  buttonType={'icon'}
+                  supported={support}
                   onClick={(): void => {
-                    history.push(`/home/artist/${username}`);
+                    history.push(`/home/artist/${username}/support`);
                   }}
-                >
-                  <div className="col s8 align-items-end">
-                    <div className="name">
-                      <span>{name}</span>
-                    </div>
-                  </div>
-                  <div className="col s4 support">
-                    <ButtonSupport
-                      buttonType={'icon'}
-                      supported={support}
-                      onClick={(): void => {
-                        history.push(`/home/artist/${username}/support`);
-                      }}
-                    />
-                  </div>
-                </div>
+                />
               </div>
             </div>
           </div>
--- src/pages/profile/artists/index.tsx
+++ src/pages/profile/artists/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { IonList, IonContent } from '@ionic/react';
-import { withRouter, RouteComponentProps } from 'react-router-dom';
+import { IonContent } from '@ionic/react';
+import { RouteComponentProps } from 'react-router-dom';
 import { CardArtist } from './../../../components';
 import { getArtistsAPI } from './../../../actions';
 import { ArtistInterface } from '../../../interfaces';
@@ -9,7 +9,6 @@ import { connect } from 'react-redux';
 
 interface StateProps {
   artists: ArtistInterface[];
-  isPlaying: boolean;
 }
 
 interface DispatchProps {
@@ -17,39 +16,31 @@ interface DispatchProps {
 }
 
 interface Props extends StateProps, DispatchProps, RouteComponentProps {}
-
 class ProfileArtistsPage extends React.Component<Props> {
   componentDidMount(): void {
     this.props.getArtistsAPI();
   }
 
   render(): React.ReactNode {
-    return (
-      <IonContent>
-        <div
-          className={
-            `profile-artists-page` + (this.props.isPlaying && ' is-playing')
-          }
-        >
-          <IonList className="artist-list">
-            {this.props.artists.map(
-              (data, i): React.ReactNode => (
-                <CardArtist key={i} artist={data} />
-              )
-            )}
-          </IonList>
-        </div>
-      </IonContent>
-    );
+    const { artists } = this.props;
+    if (artists && artists.length > 0) {
+      return (
+        <IonContent>
+          {artists.map(
+            (data, i): React.ReactNode => (
+              <CardArtist key={i} artist={data} />
+            )
+          )}
+        </IonContent>
+      );
+    }
+    return <IonContent />;
   }
 }
 
-const mapStateToProps = ({ artistAPI, settings }: ApplicationState): object => {
+const mapStateToProps = ({ artistAPI }: ApplicationState): object => {
   const { artists } = artistAPI;
-  const { isPlaying } = settings;
-  return { artists, isPlaying };
+  return { artists };
 };
 
-export default withRouter(
-  connect(mapStateToProps, { getArtistsAPI })(ProfileArtistsPage)
-);
+export default connect(mapStateToProps, { getArtistsAPI })(ProfileArtistsPage);

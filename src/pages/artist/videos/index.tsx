f6752eb0c96fd1b04206703e8ff165072a77a83a
Debora <debora.goncalves@vigil365.com>
2020-03-25 16:26:04 +0000

    https://app.clubhouse.io/vigilatg/story/431/video-archive-u1

--- src/pages/artist/videos/index.tsx
+++ src/pages/artist/videos/index.tsx
@@ -1,13 +1,41 @@
 import React from 'react';
 import { IonPage, IonContent } from '@ionic/react';
-import { HeaderOverlay, Header, BackgroundImage } from '../../../components';
+import {
+  HeaderOverlay,
+  Header,
+  BackgroundImage,
+  SliderVideo,
+  CardVideo
+} from '../../../components';
 import { validateScrollHeader } from '../../../utils';
+import { Sizes, ShapesSize, ArtistInterface } from '../../../interfaces';
+import { RouteComponentProps, withRouter } from 'react-router';
+import { connect } from 'react-redux';
+import { ApplicationState } from '../../../reducers';
+import { getArtistAPI, updateSettingsProperty } from './../../../actions';
 
-interface Props {}
+interface StateProps {
+  currentArtist: ArtistInterface | null;
+  isPlaying: boolean;
+}
+
+interface DispatchProps {
+  getArtistAPI: (username: string) => void;
+  updateSettingsProperty: (property: string, value: any) => void;
+}
+
+interface MatchParams {
+  id: string;
+}
 
 interface State {
   blur: boolean;
 }
+
+interface Props
+  extends StateProps,
+    DispatchProps,
+    RouteComponentProps<MatchParams> {}
 
 class ArtistVideosPage extends React.Component<Props, State> {
   private headerRef: React.RefObject<any> = React.createRef();
@@ -15,6 +43,21 @@ class ArtistVideosPage extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = { blur: false };
+  }
+
+  UNSAFE_componentWillReceiveProps(nextProps: Props): void {
+    if (
+      nextProps.match.params.id !== this.props.match.params.id ||
+      nextProps.currentArtist == null
+    ) {
+      this.props.getArtistAPI(nextProps.match.params.id);
+    }
+  }
+
+  componentDidMount(): void {
+    if (this.props.currentArtist == null) {
+      this.props.getArtistAPI(this.props.match.params.id);
+    }
   }
 
   handleScroll(event: CustomEvent<any>): void {
@@ -26,6 +69,7 @@ class ArtistVideosPage extends React.Component<Props, State> {
   }
 
   render(): React.ReactNode {
+    const { currentArtist } = this.props;
     return (
       <IonPage id="artist-videos-page">
         <div className="artist-videos-page">
@@ -47,7 +91,37 @@ class ArtistVideosPage extends React.Component<Props, State> {
               backgroundTopDark
               backgroundTopOpacity={0.7}
             >
-              
+              <div className="content-container">
+                {currentArtist?.videos?.recents !== undefined && (
+                  <SliderVideo
+                    data={currentArtist?.videos?.recents}
+                    title={'Recent Videos'}
+                    viewAll={false}
+                    size={Sizes.sm}
+                    type={ShapesSize.normal}
+                  />
+                )}
+                <div className="row">
+                  <div className="col s12 showcase">
+                    <h1 className="title">Showcase</h1>
+
+                    {currentArtist?.videos?.showcase.map(
+                      (value, i): React.ReactNode => {
+                        return (
+                          <CardVideo
+                            key={i}
+                            size={Sizes.lg}
+                            type={ShapesSize.full}
+                            time={value.time}
+                            video={value.video}
+                            image={value.image}
+                          />
+                        );
+                      }
+                    )}
+                  </div>
+                </div>
+              </div>
             </BackgroundImage>
           </IonContent>
         </div>
@@ -56,4 +130,18 @@ class ArtistVideosPage extends React.Component<Props, State> {
   }
 }
 
-export default ArtistVideosPage;
+const mapStateToProps = ({
+  artistAPI,
+  settings
+}: ApplicationState): StateProps => {
+  const { currentArtist } = artistAPI;
+  const { isPlaying } = settings;
+  return { currentArtist, isPlaying };
+};
+
+export default withRouter(
+  connect(mapStateToProps, {
+    getArtistAPI,
+    updateSettingsProperty
+  })(ArtistVideosPage)
+);

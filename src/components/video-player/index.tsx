1034e61f8eafc92fc7b73903e5ec6f3c188dded0
Debora <debora.goncalves@vigil365.com>
2020-03-31 16:35:11 +0100

    webkit test

--- src/components/video-player/index.tsx
+++ src/components/video-player/index.tsx
@@ -100,6 +100,7 @@ class VideoPlayerComponent extends React.Component<Props, State> {
             ref={(ref): HTMLVideoElement | null => (this.video = ref)}
             playsInline
             playsinline
+            webkit-playsinline
           >
             <source
               src="https://frontend-mocks.s3-us-west-1.amazonaws.com/mocks/videoplayback.mp4"
